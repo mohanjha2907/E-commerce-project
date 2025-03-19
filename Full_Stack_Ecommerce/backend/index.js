47507ffeba8510f77a059a7f1795573a88ca3cdd
mohanjha2907 <148763459+mohanjha2907@users.noreply.github.com>
2025-03-19 09:05:13 +0530

    Update index.js

--- Full_Stack_Ecommerce/backend/index.js
+++ Full_Stack_Ecommerce/backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection With MongoDB
-mongoose.connect("mongodb+srv://mohanjha0705:Mohanjha123274@cluster0.xgg95.mongodb.net/e-commerce");
+mongoose.connect("mongodb+srv://mohanjha0705:'your password'@cluster0.xgg95.mongodb.net/e-commerce");
 
 // paste your mongoDB Connection string above with password
 // password should not contain '@' special character
